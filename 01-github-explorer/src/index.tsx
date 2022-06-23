8b6bdf9f365bbc9ff04ef0ce400d5cc3b1727c8c
Wesley Santos <wesleysantosdev@outlook.com>
2022-06-23 19:43:43 -0300

    iniciando typescript na aplicação

---
+++ 01-github-explorer/src/index.tsx
@@ -0,0 +1,11 @@
+import { App } from './App'
+import { createRoot } from 'react-dom/client'
+import  React from 'react'
+
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>,
+);
