096393be2339b23b42d5374b88f148cfd5ca3417
Wesley Santos <wesleysantosdev@outlook.com>
2022-06-23 11:36:02 -0300

    chamadas http

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -1,11 +1,11 @@
-export function RepositoryItem(){
+export function RepositoryItem(props){
     return (
         <li>
-        <strong>unform</strong>
-        <p>Forms in React</p>
+        <strong>{props.repository.name}</strong>
+        <p>{props.repository.description}</p>
 
 
-        <a href="">
+        <a href={props.repository.html_url} target="_blank">
             Acessar repositório
         </a>
     </li>
